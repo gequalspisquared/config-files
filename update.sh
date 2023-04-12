9a102849edb97dc3b90230aca4404a6137476223
Nicholas Crane <nickcrusher30@gmail.com>
2023-04-12 13:12:35 -0400

    Create update.sh which removes old config files and copies over the current config files

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+folders=(
+    "$HOME/.config/nvim" 
+    "$HOME/.config/alacritty" 
+    "$HOME/.config/kitty" 
+    "$HOME/.config/awesome" 
+    "$HOME/.config/rofi"
+    "$HOME/.zshrc"
+)
+
+echo Removing old files;
+find . -maxdepth 1 ! -name 'update.sh' ! -name ".git" ! -name "."  -type d -exec rm -rf {} +;
+
+for i in "${folders[@]}"
+do
+    echo Updating $i;
+    cp -r $i .
+done
