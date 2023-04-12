49adae72c16c1443ccc95673ed87f42b7d8886e9
Nicholas Crane <nickcrusher30@gmail.com>
2023-04-12 11:38:14 -0400

    Install ToggleTerm

--- nvim/lua/nicholas/packer.lua
+++ nvim/lua/nicholas/packer.lua
@@ -42,4 +42,8 @@ return require('packer').startup(function(use)
       }
   }
 
+  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+      require("toggleterm").setup()
+  end}
+
 end)
