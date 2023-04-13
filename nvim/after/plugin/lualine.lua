c63cee4b93caf88722ba30ced0d450de1de40a6a
Nicholas Crane <nickcrusher30@gmail.com>
2023-04-12 21:54:47 -0400

    Install patched font

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -23,7 +23,7 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'filetype'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
