89f0c77a653b8e328ed1fc0bcac046973d5a8cb1
Nicholas Crane <nickcrusher30@gmail.com>
2023-04-18 19:05:12 -0400

    Update colors in nvim

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,12 +2,16 @@ function ColorMyPencils(color)
 	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "bg" })
 	-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	--
  --    vim.cmd.highlight('SignColumn guibg=NONE')
  --    vim.cmd.highlight('NvimTreeNormal guibg=NONE ctermbg=NONE')
 end
+
+require('tokyonight').setup({
+    dim_inactive = true,
+})
 
 ColorMyPencils()
